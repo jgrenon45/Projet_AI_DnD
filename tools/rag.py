de1d51107ec12faa744c263f1b7f06a9bc04a585
jgrenon45-logix <jgrenon@cpqlogix.com>
2025-12-04 22:13:36 -0500

    feat: implement RagAgent class for PDF processing and Chroma integration

--- tools/rag.py
+++ tools/rag.py
@@ -6,7 +6,13 @@ Processes PDFs and enables semantic search
 import os
 from pathlib import Path
 from typing import List, Optional
-import pickle
+import shutil
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_core.documents import Document
+from langchain_chroma import Chroma
+from langchain_openai import OpenAIEmbeddings
+from typing import Any
 
 # PDF processing
 try:
@@ -21,6 +27,8 @@ import chromadb
 
 # Embeddings
 from sentence_transformers import SentenceTransformer
+
+CHROMA_PATH = "./chroma_db"
 
 
 class DocumentProcessor:
@@ -282,3 +290,56 @@ class RAGSystem:
             return "Aucune règle trouvée. Assure-toi que les documents sont indexés."
         
         return f"Règles trouvées:\n\n{context}"
+
+
+# Class de Jer pour le rag
+class RagAgent:
+
+    def __init__(self, file_path: str):
+        documents = self._load_documents_from_pdf(file_path)
+        chunks = self._split_text(documents)
+        self._save_to_chroma(chunks)
+
+    def _get_embedding_function(self):
+        embeddings = OpenAIEmbeddings(
+            base_url="http://127.0.0.1:1234/v1",
+            api_key="lm-studio",
+            check_embedding_ctx_length=False
+        )
+        return embeddings
+
+    def _load_documents_from_pdf(self, file_path: str) -> Any:
+        loader = PyPDFLoader(file_path)
+        documents = loader.load()
+        return documents
+
+    def _split_text(self, documents: list[Document]):
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=500,
+            chunk_overlap=100,
+            length_function=len,
+            add_start_index=True
+        )
+        chunks = text_splitter.split_documents(documents)
+        print(f"Split {len(documents)} documents into {len(chunks)} chunks")
+
+        return chunks
+
+    def _save_to_chroma(self, chunks: list[Document]):
+
+        if os.path.exists(CHROMA_PATH):
+            shutil.rmtree(CHROMA_PATH)
+        self.db = Chroma(
+            embedding_function=self._get_embedding_function(),
+            persist_directory=CHROMA_PATH
+        )
+        self.db.add_documents(chunks)
+        print(f"Saved {len(chunks)} chunks to Chroma at {CHROMA_PATH}")
+
+    def query(self, query_text: str):
+        results = self.db.similarity_search_with_relevance_scores(query_text, k=3)
+        if len(results) == 0:
+            print("No relevant results found.")
+            return
+        context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
+        return context_text
